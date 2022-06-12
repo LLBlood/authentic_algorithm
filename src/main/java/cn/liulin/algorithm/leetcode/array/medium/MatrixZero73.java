e7450c3a85658120523b11d2ffb052dcb04c8137
liulin@51carfi.com <liulin@51carfi.com>
2022-06-12 20:43:14 +0800

    task#73. 矩阵置零

--- src/main/java/cn/liulin/algorithm/leetcode/array/medium/MatrixZero73.java
+++ src/main/java/cn/liulin/algorithm/leetcode/array/medium/MatrixZero73.java
@@ -32,4 +32,45 @@ public class MatrixZero73 {
             }
         }
     }
+
+    public void setZeroes2(int[][] matrix) {
+        boolean zeroRow = false;
+        boolean zeroCol = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                zeroCol = true;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                zeroRow = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (zeroCol) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (zeroRow) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
 }
