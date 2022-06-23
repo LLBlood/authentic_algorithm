eb4199e4372e0be7eedfb16313b0add55fd312b2
liulin <liulin@51carfi.com>
2022-06-23 18:35:07 +0800

    task# 189. 轮转数组

---
+++ src/main/java/cn/liulin/algorithm/leetcode/array/medium/RotationArray189.java
@@ -0,0 +1,16 @@
+package cn.liulin.algorithm.leetcode.array.medium;
+
+/**
+ * cn.liulin.algorithm.leetcode.array.medium$
+ * 189. 轮转数组
+ * @author ll
+ * @date 2022-06-23 18:01:02
+ **/
+public class RotationArray189 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if (k > 0) {
+
+        }
+    }
+}
