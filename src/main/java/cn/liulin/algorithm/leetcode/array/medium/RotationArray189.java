5926aaf1c66481009bd88de2f15e7ce162556367
liulin@51carfi.com <liulin@51carfi.com>
2022-06-23 21:47:58 +0800

    task# 189. 轮转数组

--- src/main/java/cn/liulin/algorithm/leetcode/array/medium/RotationArray189.java
+++ src/main/java/cn/liulin/algorithm/leetcode/array/medium/RotationArray189.java
@@ -10,7 +10,30 @@ public class RotationArray189 {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
         if (k > 0) {
+            int[] newNums = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                int z = (i + k) % nums.length;
+                newNums[z] = nums[i];
+            }
+            System.arraycopy(newNums, 0, nums, 0, newNums.length);
+        }
 
+    }
+
+    public void rotate2(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
         }
     }
 }
