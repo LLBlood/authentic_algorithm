e31d48f5a6b10f44ea5930abfee63a1edeb26d7e
liulin@51carfi.com <liulin@51carfi.com>
2022-06-27 22:27:30 +0800

    task# 567. 字符串的排列

---
+++ src/main/java/cn/liulin/algorithm/leetcode/string/medium/Arrangement567.java
@@ -0,0 +1,35 @@
+package cn.liulin.algorithm.leetcode.string.medium;
+
+import java.util.Arrays;
+
+/**
+ * cn.liulin.algorithm.leetcode.string.medium$
+ * 567. 字符串的排列
+ * @author ll
+ * @date 2022-06-27 22:21:34
+ **/
+public class Arrangement567 {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; ++i) {
+            ++cnt1[s1.charAt(i) - 'a'];
+            ++cnt2[s2.charAt(i) - 'a'];
+        }
+        if (Arrays.equals(cnt1, cnt2)) {
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            ++cnt2[s2.charAt(i) - 'a'];
+            --cnt2[s2.charAt(i - n) - 'a'];
+            if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
