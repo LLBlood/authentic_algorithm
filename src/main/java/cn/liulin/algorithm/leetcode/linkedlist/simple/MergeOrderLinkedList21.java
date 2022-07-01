ad5b94bfd02bdc3c021de7e30a17f35b0c8782ef
liulin <liulin@51carfi.com>
2022-07-01 15:47:17 +0800

    task# 21. 合并两个有序链表

--- src/main/java/cn/liulin/algorithm/leetcode/linkedlist/simple/MergeOrderLinkedList21.java
+++ src/main/java/cn/liulin/algorithm/leetcode/linkedlist/simple/MergeOrderLinkedList21.java
@@ -50,4 +50,20 @@ public class MergeOrderLinkedList21 {
         prey.next = list1 == null ? list2 : list1;
         return prehead.next;
     }
+
+    public ListNode mergeTwoLists4(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val > list2.val) {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        } else {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+    }
 }
