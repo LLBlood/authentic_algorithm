facba9ac9739bc16ec66b811e91286a5c595291a
liulin@51carfi.com <liulin@51carfi.com>
2022-07-04 20:33:08 +0800

    task#120. 三角形最小路径和

--- src/main/java/cn/liulin/algorithm/leetcode/algorithm/medium/TriangleMinimum120.java
+++ src/main/java/cn/liulin/algorithm/leetcode/algorithm/medium/TriangleMinimum120.java
@@ -12,42 +12,20 @@ import java.util.List;
  **/
 public class TriangleMinimum120 {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle == null || triangle.isEmpty()) {
-            return 0;
-        } else if (triangle.size() == 1) {
-            return triangle.get(0).get(0);
-        }
-        List<List<Integer>> list = new ArrayList<>(triangle.size());
-        list.add(triangle.get(0));
-        for (int i = 1; i < triangle.size(); i++) {
-            List<Integer> integers1 = triangle.get(i);
-            List<Integer> tempList = new ArrayList<>(integers1.size());
-            list.add(tempList);
-            List<Integer> preList = list.get(i - 1);
-            if (i < triangle.size() - 1) {
-                for (int j = 0; j < integers1.size(); j++) {
-                    Integer integer = integers1.get(j);
-                    if (j > 0 && preList.size() > j) {
-                        tempList.add(Math.min(preList.get(j - 1) + integer, preList.get(j) + integer));
-                    } else {
-                        tempList.add(preList.get(0) + integer);
-                    }
-                }
-            } else {
-                Integer min1 = Collections.min(integers1);
-                int i1 = integers1.indexOf(min1);
-                Integer min2 = Collections.min(preList);
-                int i2 = preList.indexOf(min2);
-                int min4 = Math.min(min2 + integers1.get(i2), min2 + integers1.get(i2 + 1));
-                if (i1 > 0 && preList.size() > i1) {
-                    int min3 = Math.min(preList.get(i1) + min1, preList.get(i1 - 1) + min1);
-                    return Math.min(min3, min4);
-                } else {
-                    int min3 = preList.get(i1 - 1) + min1;
-                    return Math.min(min3, min4);
-                }
+        int n = triangle.size();
+        int[][] f = new int[n][n];
+        f[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            f[i][0] = f[i - 1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; ++j) {
+                f[i][j] = Math.min(f[i - 1][j - 1], f[i - 1][j]) + triangle.get(i).get(j);
             }
+            f[i][i] = f[i - 1][i - 1] + triangle.get(i).get(i);
         }
-        return 0;
+        int minTotal = f[n - 1][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[n - 1][i]);
+        }
+        return minTotal;
     }
 }
