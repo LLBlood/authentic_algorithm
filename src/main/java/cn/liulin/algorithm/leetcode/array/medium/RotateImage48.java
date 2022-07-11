a08d9933786bafb4ed04df3057f47545124f6405
liulin <liulin@51carfi.com>
2022-07-11 16:25:16 +0800

    task# 48.旋转图像

---
+++ src/main/java/cn/liulin/algorithm/leetcode/array/medium/RotateImage48.java
@@ -0,0 +1,52 @@
+package cn.liulin.algorithm.leetcode.array.medium;
+
+/**
+ * cn.liulin.algorithm.leetcode.array.medium$
+ * 48.旋转图像
+ * @author ll
+ * @date 2022-07-11 15:03:57
+ **/
+public class RotateImage48 {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length - 1;
+        int i = 0;
+        int z = matrix.length / 2;
+        while (i < z) {
+            for (int j = i; j <= m - i - 1; j++) {
+                int temp = matrix[j][m - i];
+                matrix[j][m - i] = matrix[i][j];
+                int temp2 = matrix[m - i][m - j];
+                matrix[m - i][m - j] = temp;
+                temp = matrix[m - j][i];
+                matrix[m - j][i] = temp2;
+                matrix[i][j] = temp;
+            }
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        new RotateImage48().rotate(matrix);
+    }
+
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
