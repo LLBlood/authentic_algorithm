631976de394dcc83fa821952a65bb7d55c6de538
liulin <liulin@51carfi.com>
2022-07-29 18:03:51 +0800

    task# 48.旋转图像

--- src/main/java/cn/liulin/algorithm/leetcode/array/medium/RotateImage48.java
+++ src/main/java/cn/liulin/algorithm/leetcode/array/medium/RotateImage48.java
@@ -49,4 +49,23 @@ public class RotateImage48 {
             }
         }
     }
+
+    public void rotate3(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 }
