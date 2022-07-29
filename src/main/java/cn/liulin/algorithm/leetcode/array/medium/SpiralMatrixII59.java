2d829079f90eeae3fe026de47ec9c693edad45fb
liulin <liulin@51carfi.com>
2022-07-29 18:21:50 +0800

    task#59. 螺旋矩阵 II

--- src/main/java/cn/liulin/algorithm/leetcode/array/medium/SpiralMatrixII59.java
+++ src/main/java/cn/liulin/algorithm/leetcode/array/medium/SpiralMatrixII59.java
@@ -64,4 +64,25 @@ public class SpiralMatrixII59 {
         int[][] ints = new SpiralMatrixII59().generateMatrix(1);
         System.out.println(JSON.toJSONString(ints));
     }
+
+    public int[][] generateMatrix2(int n) {
+        int maxNum = n * n;
+        int curNum = 1;
+        int[][] matrix = new int[n][n];
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 右下左上
+        int directionIndex = 0;
+        while (curNum <= maxNum) {
+            matrix[row][column] = curNum;
+            curNum++;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n || matrix[nextRow][nextColumn] != 0) {
+                directionIndex = (directionIndex + 1) % 4; // 顺时针旋转至下一个方向
+            }
+            row = row + directions[directionIndex][0];
+            column = column + directions[directionIndex][1];
+        }
+        return matrix;
+    }
+
 }
