73613b807114fc23f568f94676ac8dedc1a4d32b
liulin <liulin@51carfi.com>
2022-08-05 10:18:15 +0800

    task#142. 环形链表 II

--- README.md
+++ README.md
@@ -1574,6 +1574,14 @@ S=|(x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1)|/2
 
 自定义：循环进位
 
+#### 3.2.8 第142题. 环形链表 II
+
+代码存放路径：cn.liulin.algorithm.leetcode.linkedlist.medium.CircularII142
+
+解题方法：
+
+自定义：循环进位
+
 ### 3.4 栈\队列
 
 #### 3.4.1 第20题. 有效的括号
---
+++ src/main/java/cn/liulin/algorithm/leetcode/linkedlist/medium/CircularII142.java
@@ -0,0 +1,46 @@
+package cn.liulin.algorithm.leetcode.linkedlist.medium;
+
+import cn.liulin.algorithm.leetcode.pojo.circular.ListNode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * cn.liulin.algorithm.leetcode.linkedlist.medium$
+ * 142. 环形链表 II
+ * @author ll
+ * @date 2022-08-05 09:51:28
+ **/
+public class CircularII142 {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            } else {
+                set.add(head);
+                head = head.next;
+            }
+        }
+        return null;
+    }
+
+    public ListNode detectCycle2(ListNode head) {
+        Map<ListNode, Integer> map = new HashMap<>();
+        while (head != null) {
+            if (map.containsKey(head)) {
+                return head;
+            } else {
+                map.put(head, 1);
+                head = head.next;
+            }
+        }
+        return null;
+    }
+
+    public ListNode detectCycle3(ListNode head) {
+
+    }
+}
